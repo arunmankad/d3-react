5005d5bd0f1435c9c14ffc66635ab27860c740a5
Arun M <arunm@Aruns-MacBook-Air.local>
2020-08-25 13:18:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import ForceLayout from './components/ForceLayout';
+
+const dataset = {
+  "nodes": [
+    {"id": 1, "name": "Gen 2 Life","type": "product"},
+    {"id": 2, "name": "Betty", "type": "property"},
+    {"id": 3, "name": "Cate", "type": "property"},
+    {"id": 4, "name": "Dave", "type": "property"},
+    {"id": 5,"name": "Ellen","type": "rule"},
+    { "id": 6, "name": "Fiona", "type": "rule"},
+    {"id": 7,"name": "Garry","type": "rule"},
+    {"id": 8,"name": "Holly","type": "rule"},
+    {"id": 9,"name": "Iris","type": "rule"},
+    {"id": 10,"name": "Jane","type": "rule"}
+  ],
+  "links": [
+    {"source": 1,"target": 2},
+    {"source": 1,"target": 3},
+    {"source": 1,"target": 4},
+    {"source": 2,"target": 5},
+    {"source": 2,"target": 6},
+    {"source": 3,"target": 7},
+    {"source": 3,"target": 8},
+    {"source": 4,"target": 9},
+    {"source": 4,"target": 10}
+  ]
+};
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+  const [width, setWidth] = useState(1000);
+  const [height, setHeight] = useState(300);
+
+	useEffect(() => {
+    console.log('Use Effect in App', window.innerWidth);
+    // window.addEventListener('resize', updateWindowDimensions);
+		setData(dataset);
+	}, []);
+  const updateWindowDimensions = () => {
+    alert(window.innerWidth)
+  }
+	function deepCopy(obj){
+		return JSON.parse(JSON.stringify(obj));
+  }
+  
+  const nodeHoverTooltip =(node)=>{
+    console.log(`this is the tool tip hover function ${node}`);
+    return `<div>${node.name}</div>`
+  }
+
+	const updateData1 = () => {
+		var _data = deepCopy(data);
+		_data.nodes.push(
+      {
+        "id": 3,
+        "name": "Arun",
+        "gender": "male"
+      }
+    )
+		console.log("updateData1", _data);
+
+		setData(_data);
+	}
+
+	const updateData2 = () => {
+		var _data = deepCopy(data);
+		
+		_data["children"][0]["children"].push({
+			"name":"mister_p",
+			"group":"C",
+			"value":20,
+			"colname":"level3"
+		})
+
+		_data["children"][2]["children"].splice(2,1);
+
+		setData(_data);
+	}
+
+	const updateData3 = () => {
+		var _data = deepCopy(data);
+
+		_data["children"].push({
+			"name": "boss4",
+			"children": [{
+				"name":"mister_z",
+				"group":"E",
+				"value":40,
+				"colname":"level3"
+			}]
+		});
+
+		setData(_data);
+	}
+
+	const updateData4 = () => {
+		var _data = deepCopy(data);
+
+		_data["children"].splice(1,1);
+
+		setData(_data);
+	}
+
+	const resetData = () => {
+		setData(dataset);
+	}
+
+	if(data === null) return <></>;
+
+	return (
+		<div className="App">
+			<h2>Graphs with React</h2>
+			<div className="btns">
+				<button onClick={updateData1}>Add 1 more Node</button>
+				<button onClick={updateData2}>Add/Remove Child Nodes</button>
+				<button onClick={updateData3}>Add Parent Nodes</button>
+				<button onClick={updateData4}>Remove Parent Nodes</button>
+				<button onClick={resetData}>Reset</button>
+			</div>
+			<ForceLayout data={data} nodeHoverTooltip={nodeHoverTooltip}  />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/ForceLayout.js
@@ -0,0 +1,214 @@
+import * as d3 from 'd3';
+import React, { useRef, useEffect } from 'react';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import {createContextMenu} from "./utils";
+// import  "./ForecLayout.css";
+import styles from "./forceGraph.module.css";
+import { style } from 'd3';
+// import { style } from 'd3';
+function ForceLayout({data, nodeHoverTooltip}){
+    const ref = useRef();
+    
+    useEffect(() => {
+        
+    }, []);
+    useEffect(()=>{
+        draw();
+    },[data]);
+    const draw = ()=>{
+        d3.selectAll('svg').remove();
+        console.log("DRAW METHOD", data)
+        const containerRect = ref.current.getBoundingClientRect();
+        const height = containerRect.height;
+        const width = containerRect.width;
+        const links = data.links.map((d) => Object.assign({}, d));
+        const nodes = data.nodes.map((d) => Object.assign({}, d));
+        
+        console.log("DRAW METHOD LINKS", links)
+        console.log("DRAW METHOD NODES", nodes)
+        const menuItems = [
+            {
+              title: 'First action',
+              action: (d) => {
+                // TODO: add any action you want to perform
+                console.log(d);
+              }
+            },
+            {
+              title: 'Second action',
+              action: (d) => {
+                // TODO: add any action you want to perform
+                console.log(d);
+              }
+            }
+          ];
+        
+
+        console.log("height - ",height)
+
+        const color = () => { return "#00C1D4"; };
+
+        const icon = (d) => {
+            switch (d.type){
+                case 'product':
+                    return "\uf187";
+                case 'property':
+                    return "\uf013";
+                case 'rule':
+                    return "\uf515";
+                default:
+                    return "\uf515";
+            }
+            
+        }
+
+        const getClass = (d) => {
+            return d.type === "product" ? styles.product : styles.properties;
+        };
+        const drag = (simulation) => {
+            const dragstarted = (d) => {
+              if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+              d.fx = d.x;
+              d.fy = d.y;
+            };
+        
+            const dragged = (d) => {
+              d.fx = d3.event.x;
+              d.fy = d3.event.y;
+            };
+        
+            const dragended = (d) => {
+              if (!d3.event.active) simulation.alphaTarget(0);
+              d.fx = null;
+              d.fy = null;
+            };
+        
+            return d3
+              .drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended);
+          };
+
+          const tooltip = document.querySelector("#graph-tooltip");
+          if (!tooltip) {
+              const tooltipDiv = document.createElement("div");
+              tooltipDiv.classList.add(styles.tooltip);
+              tooltipDiv.style.opacity = "0";
+              tooltipDiv.id = "graph-tooltip";
+              document.body.appendChild(tooltipDiv);
+          }
+        const div = d3.select("#graph-tooltip");
+
+        const addTooltip = (hoverTooltip, d, x, y) => {
+            console.log("X",x);
+            console.log("Y",y);
+            div
+            .transition()
+            .duration(200)
+            .style("opacity", 0.9);
+            div
+            .html(hoverTooltip(d))
+            .style("left", `${x}px`)
+            .style("top", `${y - 28}px`);
+        };
+
+        const removeTooltip = () => {
+            div
+            .transition()
+            .duration(200)
+            .style("opacity", 0);
+        };
+
+        const simulation = d3
+            .forceSimulation(nodes)
+            .force("link", d3.forceLink(links).id(d => d.id))
+            .force("charge", d3.forceManyBody().strength(-150))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY());
+  
+        const svg = d3
+            .select(ref.current)
+            .append("svg")
+            .attr("id", "graphSvg")
+            .attr("viewBox", [-width / 2, -height / 2, width, height])
+            // .call(d3.zoom().on("zoom", function () {
+            //   svg.attr("transform", d3.event.transform);
+            // }));
+
+        const link = svg
+            .append("g")
+            .attr("stroke", "#999")
+            .attr("stroke-opacity", 0.6)
+            .selectAll("line")
+            .data(links)
+            .join("line")
+            .attr("stroke-width", d => Math.sqrt(d.value));
+
+        const node = svg
+            .append("g")
+            .attr("stroke", "#cccccc")
+            .attr("stroke-width", 2)
+            .selectAll("circle")
+            .data(nodes)
+            .join("circle")
+            .on('contextmenu', (d) => {
+            createContextMenu(d, menuItems, width, height, '#graphSvg');
+            })
+            .attr("r", 15)
+            .attr("fill", color)
+            .call(drag(simulation));
+
+        const label = svg.append("g")
+            .attr("class", "labels")
+            .selectAll("text")
+            .data(nodes)
+            .enter()
+            .append("text")
+            .on('contextmenu', (d) => {
+            createContextMenu(d, menuItems, width, height, '#graphSvg');
+            })
+            .attr('text-anchor', 'middle')
+            .attr('dominant-baseline', 'central')
+            .attr("class", d => `fa ${getClass(d)}`)
+            .text(d => {return icon(d);})
+            .call(drag(simulation));
+
+    label.on("mouseover", (d) => {
+        console.log("pageX",d3.event.pageX);
+        console.log("pageY",d3.event.pageY);
+        addTooltip(nodeHoverTooltip, d, d3.event.pageX, d3.event.pageY);
+    })
+    .on("mouseout", () => {
+      removeTooltip();
+    });
+
+  simulation.on("tick", () => {
+    //update link positions
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
+
+    // update node positions
+    node
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
+
+    // update label positions
+    label
+      .attr("x", d => { return d.x; })
+      .attr("y", d => { return d.y; })
+  });
+}    // end of draw method
+    return(
+        <div className="chart">
+            <p>ForecLayout</p>
+            <div ref={ref} className={styles.container} >
+
+            </div>
+        </div>
+    )
+}
+export default ForceLayout;
